7dc9dad3480d7ac545412cc4fc3c4c7594ac6f51
shrkoo <shrko3062@gmail.com>
2026-08-15 03:43:02 +0330

    Create MainActivity.java

---
+++ app/app/src/main/app/src/main/java/ai/homa/app/MainActivity.java
@@ -0,0 +1,41 @@
+package ai.homa.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    private static final String HOMA_AI_URL =
+            "https://capable-aria-chat-flow.base44.app";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(HOMA_AI_URL);
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
